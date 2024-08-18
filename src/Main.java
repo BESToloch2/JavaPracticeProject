3ec4ee105839eb009db352556f11f5838790361a
BESToloch2 <befanalex2@gmail.com>
2024-08-18 02:06:01 -0500

    finished first video class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("It`s really good");
+        System.out.println();
+        //This is a comment
+        /*
+        This
+        is
+        a
+        comment
+         */
+
+
+
+
     }
 }
